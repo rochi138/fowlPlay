0a39869497c99e608bbceaf907a2ab34236c203b
Sorano <sabrinayu7@gmail.com>
2019-08-30 15:12:05 -0400

    fixes Goose and Player_Controller scripts, moves goose horizontally

--- honkhonk/Assets/Scripts/Player_Control.cs
+++ honkhonk/Assets/Scripts/Player_Control.cs
@@ -2,57 +2,90 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player_Control : PhysicsObject
+public class Player_Control : MonoBehaviour
 {
+    [Range(0, .3f)] [SerializeField] private float movementSmoothing = .05f;	// How much to smooth out the movement
 
     public float maxSpeed = 7;
     public float jumpTakeOffSpeed = 7;
     public float momentum;
+    public float horizontalMove = 0f;
 
     private SpriteRenderer spriteRenderer;
     private Animator animator;
+    private Rigidbody2D playerRigidbody;
+    private bool facingRight = true;
+    private Vector3 playerVelocity = Vector3.zero;
 
     // Use this for initialization
     void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
         animator = GetComponent<Animator>();
+        playerRigidbody = GetComponent<Rigidbody2D>();
     }
 
-    protected override void ComputeVelocity()
-    {
-        animator.SetBool("Walk", false);
-        Vector2 move = Vector2.zero;
-        momentum = velocity.x;
-        if (Input.GetButtonDown("Jump"))
-        {
-            velocity.y = jumpTakeOffSpeed;
-        }
-        move.x = Input.GetAxis("Horizontal");
+    //protected override void ComputeVelocity()
+    //{
+    //    animator.SetBool("Walk", false);
+    //    Vector2 move = Vector2.zero;
+    //    momentum = velocity.x;
+    //    if (Input.GetButtonDown("Jump"))
+    //    {
+    //        velocity.y = jumpTakeOffSpeed;
+    //    }
+    //    move.x = Input.GetAxis("Horizontal");
 
-        if (move.x != 0)
-            {
-            animator.SetBool("Walk", true);
-        }
+    //    if (move.x != 0)
+    //        {
+    //        animator.SetBool("Walk", true);
+    //    }
 
 
-        bool flipSprite = (spriteRenderer.flipX ? (move.x > 0.01f) : (move.x < 0.01f));
-        if (flipSprite)
-        {
-            spriteRenderer.flipX = !spriteRenderer.flipX;
-        }
+    //    bool flipSprite = (spriteRenderer.flipX ? (move.x > 0.01f) : (move.x < 0.01f));
+    //    if (flipSprite)
+    //    {
+    //        spriteRenderer.flipX = !spriteRenderer.flipX;
+    //    }
 
-        animator.SetBool("grounded", grounded);
+    //    animator.SetBool("grounded", grounded);
         
 
-        targetVelocity = move * maxSpeed;
-        if (grounded == false)
+    //    targetVelocity = move * maxSpeed;
+    //    if (grounded == false)
+    //    {
+    //        targetVelocity.x = momentum;
+    //    }
+    //    else
+    //    {
+    //        momentum = 0;
+    //    }
+    //}
+
+    public void Move(float move)
+    {
+        //find target velocity
+        Vector3 targetVelocity = new Vector2(move * 10f, playerRigidbody.velocity.y);
+        playerRigidbody.velocity = Vector3.SmoothDamp(playerRigidbody.velocity, targetVelocity, ref playerVelocity, movementSmoothing);
+
+        if (move > 0 && !facingRight)
         {
-            targetVelocity.x = momentum;
+            Flip();
         }
-        else
+        else if (move < 0 && facingRight)
         {
-            momentum = 0;
+            Flip();
         }
+
+    }
+
+    public void Flip()
+    {
+        // Change player orientation
+        facingRight = !facingRight;
+
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
     }
 }
