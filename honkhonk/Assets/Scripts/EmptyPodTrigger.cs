00dbd965a3a7d5f5322e1c72a68fe99d958fface
Sorano <sabrinayu7@gmail.com>
2019-12-31 15:52:18 -0500

    Created fade animations

---
+++ honkhonk/Assets/Scripts/EmptyPodTrigger.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmptyPodTrigger : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        var objectName = other.gameObject.name;
+        GameObject strikingBird = other.gameObject;
+        if (strikingBird.GetComponent<Player_Control>().getHeadbutt() == true)
+        {
+
+        }
+
+        //ContactPoint2D = ;
+
+        //gother.contacts 
+        
+
+
+        Debug.Log("Collided with " + name);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ honkhonk/Assets/Scripts/LvlChangerScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class LvlChangerScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Animator animator;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
