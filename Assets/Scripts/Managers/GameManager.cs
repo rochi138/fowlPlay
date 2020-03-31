3e0465382a7ab1c4a6442590625f510c0478448d
rochi138 <robyn.ching138@gmail.com>
2020-03-31 02:27:51 -0400

    Removed old game manager

--- Assets/Scripts/Managers/GameManager.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class GameManager : MonoBehaviour {
-    public static AudioManager AudioManager = new AudioManager();
-    public static InventoryManager InventoryManager = new InventoryManager();
-    public static SceneController SceneController = new SceneController();
-    public static StateManager StateManager = new StateManager();
-}
