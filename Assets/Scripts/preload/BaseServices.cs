4c720d21bf605131a48fe23e138592f98c748ab7
rochi138 <robyn.ching138@gmail.com>
2020-04-30 18:18:54 -0400

    Added BaseServices class

--- Assets/Scripts/preload/BaseServices.cs
+++ Assets/Scripts/preload/BaseServices.cs
@@ -6,19 +6,24 @@ namespace ServiceLocator {
     public interface IBaseService : IGameService { 
         IBaseService GetInstance();
     }
+
+    public class BaseService : IBaseService {
+        private static BaseService instance_ = null;
+        public IBaseService GetInstance() { return (IBaseService)instance_; }
+        public BaseService() {
+            if ( instance_ == null )
+                instance_ = this;
+        }
+    }
+
     public interface IAudioService : IGameService {
         void playSound(int soundID);
         void stopSound(int soundID);
         void stopAllSounds();
     }
 
-    public class AudioBase : IAudioService, IBaseService {
-        private static AudioBase instance_ = null;
-        public IBaseService GetInstance() { return (IBaseService)instance_; }
-        public AudioBase() {
-            if ( instance_ == null )
-                instance_ = this;
-        }
+    public class AudioBase : BaseService, IAudioService {
+        public AudioBase() : base() { }
         public void playSound(int soundID) { }
         public void stopSound(int soundID) { }
         public void stopAllSounds() { }
