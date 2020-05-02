8667c1875d4c7d2b178a98b5c2738b1141541e2b
Sabrina Yu <sabrinayu7@gmail.com>
2020-05-02 14:20:25 -0400

    created base InputHandler and Command classes for Command design pattern

---
+++ Assets/Scripts/Command.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public abstract class Command : MonoBehaviour
+{
+    // how far character moves
+    protected float moveDistance = 1f;
+
+    public abstract void Execute(Transform charTrans, Command command);
+
+    public virtual void Move(Transform charTrans) { }
+
+}
+
+// -- Child classes -- //
+    
+public class MoveForward : Command
+{
+    public override void Execute(Transform charTrans, Command command)
+    {
+        Move(charTrans);
+    }
+
+    public override void Move(Transform charTrans)
+    {
+        charTrans.Translate(charTrans.forward * moveDistance);
+    }
+}
+
+public class MoveBackward : Command
+{
+    public override void Execute(Transform charTrans, Command command)
+    {
+        Move(charTrans);
+    }
+
+    public override void Move(Transform charTrans)
+    {
+        charTrans.Translate(-charTrans.forward * moveDistance);
+    }
+}
+
+
---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class InputHandler : MonoBehaviour
+
+{
+    // Char we controlling with keys
+    public Transform charTrans; 
+
+    // keys needed
+    private Command buttonLeft, buttonRight;
+
+
+    // Start is called before the first frame update
+    protected abstract void Start();
+
+    // Update is called once per frame
+    protected abstract void Update();
+
+    public void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            buttonLeft = new MoveLeft();
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            buttonRight = new MoveRight();
+        }
+
+    }
+}
